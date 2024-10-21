c33bdf73d5852b2923f48a75e94d4e30b6f79de1
Radja Restu Arsita <158986576+Radja-Restu-A@users.noreply.github.com>
2024-10-21 22:30:10 +0700

    Update payment gateway v3
    - bisa mengririm banyak data produk
    - auto update database ketika pembayaran sudah dilakukan
    - pengubahan pengambilan data untuk redirect_url menjadi mengambil data token

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -5,58 +5,72 @@ namespace App\Http\Controllers;
 use App\Models\Transaction;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
+use App\Models\Order;
 use App\Models\OrderItem;
 use App\Models\Product;
+
 class PaymentController extends Controller
 {
-    
     public function create (Request $request){
-        $order = OrderItem::find($request->order_id);
+        $nama = [];
+        $order_item = [];
+        $grossAmount = 0;
+        
+        $order = Order::find($request->order_id);
         if (!$order) {
             return response()->json(['error' => 'Order not found'], 404);
         }
-
-        $nama = Product::find($order->product_id);
-        if (!$nama) {
-            return response()->json(['error' => 'Product not found'], 404);
+        
+        $valid = OrderItem::where('order_id', $order->id)->get();
+        if ($valid->isEmpty()) {
+            return response()->json(['error' => 'order item not found'], 404);
+            for($i = 0; $i < $valid->count(); $i++) {
+                $order_item[$i] = $valid->skip($i)->first();
+                $nama[$i] = Product::find($order_item[$i]->product_id);
+            }
         }
 
         $params = array (
             'transaction_details' => array(
                 'order_id' => $request->test,
-                'gross_amount' =>  $order->price * $order->quantity * 1000,
+                'gross_amount' =>  $order->total_amount * 1000,
             ),
             
             'customer_details' => array(
                 'first_name' => $order->customer_name,
                 'email' => 'dummy@gmail.com',
             ),
-            'item_details' => array(
-                array(
-                    'name' => $nama->name,
-                    'price' => $order->price * 1000,
-                    'quantity' => $order->quantity,
-                )
-            ),
+            'item_details' => array(),
             'enabled_payments' => array('bca_va','bni_va','bri_va', 'gopay', 'shopeepay')
         );
+
+        for ($i = 0; $i < $valid->count(); $i++) {
+            $grossAmount = $grossAmount + $order_item[$i]->price * 1000 * $order_item[$i]->quantity;
+        }
+
+        $params['transaction_details']['gross_amount'] = $grossAmount;
         
+        for ($i = 0; $i < $valid->count(); $i++) {
+            $params['item_details'][] = array(
+                'name' => $nama[$i]->name,
+                'price' => $order_item[$i]->price * 1000,
+                'quantity' => $order_item[$i]->quantity
+            );
+        }
         
         $auth = base64_encode(env('MIDTRANS_SERVER_KEY'));
-    
+        
         $response = Http::withHeaders([
             'Content-Type' => 'application/json',
             'Authorization' => "Basic $auth",
-        ])->post('https://app.sandbox.midtrans.com/snap/v1/transactions', $params);
-
-        $response = json_decode($response->body());
+        ])->post('https://app.sandbox.midtrans.com/snap/v1/transactions', $params); 
     
         $payment = new Transaction;
         $payment->order_id = $params['transaction_details']['order_id'];
-        $payment->total_paid = $order->price * $order->quantity;
+        $payment->total_paid = $params['transaction_details']['gross_amount'];
         $payment->payment_status = 'pending';
         $payment->payment_method = '-';
-        $payment->checkout_link = $response->redirect_url;
+        $payment->checkout_link = $response->json('token');
         $payment->save();
         
         return response()->json($response);
@@ -83,6 +97,7 @@ class PaymentController extends Controller
         }
         if($response->transaction_status === 'settlement'){
             $payment->payment_status = 'settlement';
+            $payment->payment_method = $response->payment_type;
         }
         if($response->transaction_status === 'pending'){
             $payment->payment_status = 'pending';
@@ -96,7 +111,6 @@ class PaymentController extends Controller
         if($response->transaction_status === 'cancle'){
             $payment->payment_status = 'cancle';
         }
-
         $payment->save();
         return response()->json('success');
     }
